e48195b39dedeabf671592afbb4c3274954c2398
samiboukadida <samiboukadida@free.fr>
2016-12-19 12:20:46 +0100

    First Commit
    integration og Propel2 within ZF3

---
+++ module/Application/src/Module.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Application;
+
+class Module
+{
+    const VERSION = '3.0.2dev';
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/../config/module.config.php';
+    }
+
+    public function init()
+    {
+        // setup Propel
+        require_once __DIR__ . '/../../../orm/generated-conf/config.php';
+    }
+}
